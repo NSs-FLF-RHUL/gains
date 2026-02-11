c125531ef54f5edd5c7f3a6a40a854392c85a58b
willGraham01 <ccaegra@ucl.ac.uk>
2026-02-11 14:33:33 +0000

    Parametrise the tests

--- tests/test_initial_conditions/test_mcnally.py
+++ tests/test_initial_conditions/test_mcnally.py
@@ -1,35 +1,37 @@
 import numpy as np
+import pytest
 
 from gains.initial_conditions.mcnally import density
 
 
-def test_mcnally_density() -> None:
-    ys = np.zeros((10,), dtype=float)
-
-    params = {
-        "rho_1": 0.0,
-        "rho_2": 0.0,
-        "L": 1.0,
-    }
+@pytest.mark.parametrize(
+    ("ys", "params", "expected_output"),
+    [
+        pytest.param(
+            np.zeros((10,)),
+            {
+                "rho_1": 0.0,
+                "rho_2": 0.0,
+                "L": 1.0,
+            },
+            np.zeros((10,)),
+            id="Every y-coord is 0",
+        ),
+        pytest.param(
+            np.array([0.125, 0.375, 0.625, 0.875]),
+            {
+                "rho_1": 1.0,
+                "rho_2": 1.0,
+                "L": 1.0,
+            },
+            np.ones((4,)),
+            id="Midpoint of each interval",
+        ),
+    ],
+)
+def test_mcnally_density(ys, params, expected_output) -> None:
     params["rho_m"] = 0.5 * (params["rho_1"] - params["rho_2"])
 
     computed_output = density(ys, **params)
-    expected_output = np.zeros_like(ys)
-
-    assert np.allclose(computed_output, expected_output)
-
-
-def test_mcnally_density_compartments() -> None:
-    ys = np.array([0.125, 0.375, 0.625, 0.875])
-
-    params = {
-        "rho_1": 1.0,
-        "rho_2": 1.0,
-        "L": 1.0,
-    }
-    params["rho_m"] = 0.5 * (params["rho_1"] - params["rho_2"])
-
-    computed_output = density(ys, **params)
-    expected_output = np.array([1.0, 1.0, 1.0, 1.0])
 
     assert np.allclose(computed_output, expected_output)

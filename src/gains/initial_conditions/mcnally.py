09a6a0098b21829737acf4e4da6f435d4ffcc47a
willGraham01 <ccaegra@ucl.ac.uk>
2026-02-11 12:09:30 +0000

    Extract density to package submodule

--- src/gains/initial_conditions/mcnally.py
+++ src/gains/initial_conditions/mcnally.py
@@ -2,3 +2,31 @@
 Contains functions for generating initial conditions as per
 McNally et al., 2012, ApJ, 201, 18.
 """
+
+import numpy as np
+
+
+def density(ys, **parameters):
+    out = []
+    for el in ys:
+        if el < 0.25:
+            out.append(
+                parameters["rho_1"]
+                - parameters["rho_m"] * np.exp((el - 0.25) / parameters["L"])
+            )
+        elif 0.25 <= el < 0.5:
+            out.append(
+                parameters["rho_2"]
+                + parameters["rho_m"] * np.exp((-el + 0.25) / parameters["L"])
+            )
+        elif 0.5 <= el < 0.75:
+            out.append(
+                parameters["rho_2"]
+                + parameters["rho_m"] * np.exp(-(0.75 - el) / parameters["L"])
+            )
+        else:
+            out.append(
+                parameters["rho_1"]
+                - parameters["rho_m"] * np.exp(-(el - 0.75) / parameters["L"])
+            )
+    return out

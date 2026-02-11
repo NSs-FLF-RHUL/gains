38a01fe6b20f2acee4574fbf9e4480e76096d6c6
willGraham01 <ccaegra@ucl.ac.uk>
2026-02-11 14:36:25 +0000

    Provide rho_m explicitly to help test cases

--- tests/test_initial_conditions/test_mcnally.py
+++ tests/test_initial_conditions/test_mcnally.py
@@ -13,25 +13,20 @@ from gains.initial_conditions.mcnally import density
                 "rho_1": 0.0,
                 "rho_2": 0.0,
                 "L": 1.0,
+                "rho_m": 0.0,
             },
             np.zeros((10,)),
             id="Every y-coord is 0",
         ),
         pytest.param(
             np.array([0.125, 0.375, 0.625, 0.875]),
-            {
-                "rho_1": 1.0,
-                "rho_2": 1.0,
-                "L": 1.0,
-            },
+            {"rho_1": 1.0, "rho_2": 1.0, "L": 1.0, "rho_m": 0.0},
             np.ones((4,)),
             id="Midpoint of each interval",
         ),
     ],
 )
 def test_mcnally_density(ys, params, expected_output) -> None:
-    params["rho_m"] = 0.5 * (params["rho_1"] - params["rho_2"])
-
     computed_output = density(ys, **params)
 
     assert np.allclose(computed_output, expected_output)

2910f819f26e2c0b813722817234661c3ef7d0c3
Will Graham <32364977+willGraham01@users.noreply.github.com>
2026-02-12 12:00:41 +0000

    Add tests for the density function (#15)
    * Delete placeholder test
    
    * Write input/output tests for density
    
    * Parametrise the tests
    
    * Provide rho_m explicitly to help test cases
    
    * Add test case for being at interval boundaries
    
    * Add typehints
    
    * Add some negative tests
    
    * Typehints for negative test

---
+++ tests/test_initial_conditions/test_mcnally.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pytest
+
+from gains.initial_conditions.mcnally import density
+
+
+@pytest.mark.parametrize(
+    ("ys", "params", "expected_output"),
+    [
+        pytest.param(
+            np.zeros((10,)),
+            {
+                "rho_1": 0.0,
+                "rho_2": 0.0,
+                "L": 1.0,
+                "rho_m": 0.0,
+            },
+            np.zeros((10,)),
+            id="Every y-coord is 0",
+        ),
+        pytest.param(
+            np.array([0.125, 0.375, 0.625, 0.875]),
+            {
+                "rho_1": 1.0,
+                "rho_2": 1.0,
+                "L": 1.0,
+                "rho_m": 0.0,
+            },
+            np.ones((4,)),
+            id="Midpoint of each interval",
+        ),
+        pytest.param(
+            np.array([0.25, 0.5, 0.75]),
+            {
+                "rho_1": 1.0,
+                "rho_2": 10.0,
+                "L": 1.0,
+                "rho_m": 0.0,
+            },
+            np.array([10.0, 10.0, 1.0]),
+            id="On the interval boundaries",
+        ),
+    ],
+)
+def test_mcnally_density(
+    ys: np.ndarray, params: dict[str, float], expected_output: np.ndarray
+) -> None:
+    computed_output = density(ys, **params)
+
+    assert np.allclose(computed_output, expected_output)
+
+
+@pytest.fixture
+def params() -> dict[str, float]:
+    return {
+        "rho_1": 1.0,
+        "rho_2": 10.0,
+        "L": 1.0,
+        "rho_m": 0.0,
+    }
+
+
+@pytest.mark.parametrize(
+    ("missing_key",),
+    [
+        pytest.param("rho_1"),
+        pytest.param("rho_2"),
+        pytest.param("L"),
+        pytest.param("rho_m"),
+    ],
+)
+def test_mcnally_missing_params(
+    missing_key: str,
+    params: dict[str, float],
+    ys: np.ndarray = np.array([0.125, 0.375, 0.625, 0.875]),
+) -> None:
+    del params[missing_key]
+
+    with pytest.raises(KeyError, match=missing_key):
+        density(ys, **params)

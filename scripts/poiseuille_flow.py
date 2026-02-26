ecb8c0a1076928345e182ef29f44f8c0743ae589
Rebecca McShane <rmcshane29@gmail.com>
2026-02-26 14:39:22 +0000

    Removed lambdas from poiseuille_flow.py

--- scripts/poiseuille_flow.py
+++ scripts/poiseuille_flow.py
@@ -50,9 +50,13 @@ tau_1 = dist.Field(name="tau_1")
 tau_2 = dist.Field(name="tau_2")
 
 # Substitutions
-dy = lambda A: d3.Differentiate(A, ycoord)
+def dy(A):
+    return d3.Differentiate(A, ycoord)
+
 lift_basis = ybasis.derivative_basis(2)
-lift = lambda A, n: d3.Lift(A, lift_basis, n)
+
+def lift(A, n):
+    return d3.Lift(A, lift_basis, n)
 
 # Problem
 problem = d3.LBVP([u, tau_1, tau_2], namespace=locals())

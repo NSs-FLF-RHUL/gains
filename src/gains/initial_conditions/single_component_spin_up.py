3ecdd2abea35a5f61bbbdeff9d79ac5a7aef3acd
Rebecca McShane <rmcshane29@gmail.com>
2026-03-18 15:10:12 +0000

    Added precision check to remove excessively small values from initial conditions

--- src/gains/initial_conditions/single_component_spin_up.py
+++ src/gains/initial_conditions/single_component_spin_up.py
@@ -1,16 +1,19 @@
 import numpy as np
 
 
-def window(coord: np.ndarray, width: float) -> np.ndarray:
+def window(coord: np.ndarray, width: float, dtype: type) -> np.ndarray:
     """
     Creates window function for use in enforcing boundary conditions on only parts of the sphere.
 
     :param coord: Coordinate on which to create the window.
     :param width: Width of the window function.
+    :param dtype
     :returns mask: A smooth function equal to 1 in the window, and 0 everywhere else.
     """
     a = width / 2
     shift = np.pi / 2 * np.ones_like(coord)
     coord = coord - shift
     mask = np.tanh((coord + a) / 0.1) - np.tanh((coord - a) / 0.1)
+    precision = np.finfo(dtype).eps
+    mask[mask < 1e3*precision] = 0 #about 3 orders of magnitude above dtype precision limit
     return 0.5 * mask

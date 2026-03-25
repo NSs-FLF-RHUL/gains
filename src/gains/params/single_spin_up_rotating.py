3d71ed78fcc4e057048b3d28741906dbe9c78068
Rebecca McShane <rmcshane29@gmail.com>
2026-03-25 11:26:56 +0000

    Make parameters a standard rather than ordered dict, and specified typing as Any

--- src/gains/params/single_spin_up_rotating.py
+++ src/gains/params/single_spin_up_rotating.py
@@ -1,15 +1,15 @@
 """Holds the parameters for scripts/single_spin_up_rotating_frame.py."""
 
-from collections import OrderedDict
+from typing import Any
 
-parameters = OrderedDict()
+parameters = dict[str, Any]()
 parameters["Nphi"] = 256
 parameters["Ntheta"] = 128
 parameters["Nr"] = 128
 parameters["omega"] = 1
-parameters["Delta_Omega"] = 1e-3  # type: ignore[assignment]
-parameters["Ek"] = 5e-2  # type: ignore[assignment]
+parameters["Delta_Omega"] = 1e-3
+parameters["Ek"] = 5e-2
 
-parameters["dealias"] = 3 / 2  # type: ignore[assignment]
+parameters["dealias"] = 3 / 2
 parameters["stop_sim_time"] = 20
-parameters["Omega_Init"] = 1.0  # type: ignore[assignment]
+parameters["Omega_Init"] = 1.0

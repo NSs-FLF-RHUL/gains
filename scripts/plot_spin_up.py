8d711f9d3c25a0e4e70286c762d1dccbb73d8382
Rebecca McShane <rmcshane29@gmail.com>
2026-03-30 16:38:14 +0100

    Added frame directory to command line options

--- scripts/plot_spin_up.py
+++ scripts/plot_spin_up.py
@@ -27,9 +27,25 @@ parser.add_argument(
     "output_dir", type=str, default=None, help="Path to output directory."
 )
 
+parser.add_argument(
+    "--fig_dir",
+    type=str,
+    default="outputs",
+    help="The directory in which to save figures.",
+)
+
+parser.add_argument(
+    "--frame_dir",
+    type=str,
+    default="frames",
+    help="The directory in which to save frames.",
+)
+
 args = vars(parser.parse_args())
 
 logger = logging.getLogger(__name__)
+
+pathlib.Path.mkdir(pathlib.Path(args["fig_path"]), parents=True, exist_ok=True)
 
 anim_check = input("Plot frames for animation? [y/n]: ")
 
@@ -43,8 +59,9 @@ plot_angular_velocity(path_2, 40, ax[1], rotating=True)
 
 path_3 = "{}/su_equator/AZ_avg_equator/AZ_avg_equator_s4.h5".format(args["output_dir"])
 plot_angular_velocity(path_3, 90, ax[2], rotating=True)
-plt.savefig("outputs/Equator_spin_up_5e-2.png")
+plt.savefig("{}/Equator_spin_up_5e-2.png".format(args["fig_path"]))
 plt.close()
+
 path = "{}/su_equator/AZ_avg_equator".format(args["output_dir"])
 r_check, theta_check = get_angular_coords(path + "/AZ_avg_equator_s1.h5")
 
@@ -60,7 +77,7 @@ path_list = plot_against_time(r, "r", path, return_paths=return_check, name="sur
 if anim_check == "y":
     num_files = len(path_list)
     count = 0
-    pathlib.Path.mkdir(pathlib.Path("frames"), parents=True, exist_ok=True)
+    pathlib.Path.mkdir(pathlib.Path(args["frame_path"]), parents=True, exist_ok=True)
     for i in range(num_files):
         path = path_list[i]
         data = h5py.File(path, mode="r")

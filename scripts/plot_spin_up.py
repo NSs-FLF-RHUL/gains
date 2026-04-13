deb8149d9c7030795d6513683df12439fc0463fd
Rebecca McShane <rmcshane29@gmail.com>
2026-04-13 16:22:53 +0100

    Added if name == main block to scripts/plot_spin_up.py

--- scripts/plot_spin_up.py
+++ scripts/plot_spin_up.py
@@ -32,60 +32,61 @@ if args["parameter_file"] is not None:
 else:
     PARAMS = default_params
 
-args["output_dir"] = Path(args["output_dir"])
-args["frame_dir"] = Path(args["frame_dir"])
+if __name__ == "__main__":
+    args["output_dir"] = Path(args["output_dir"])
+    args["frame_dir"] = Path(args["frame_dir"])
 
-logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
-Path.mkdir(Path(args["fig_dir"]), parents=True, exist_ok=True)
+    Path.mkdir(Path(args["fig_dir"]), parents=True, exist_ok=True)
 
-anim_check = input("Plot frames for animation? [y/n]: ")
+    anim_check = input("Plot frames for animation? [y/n]: ")
 
-fig, ax = plt.subplots(1, 3, figsize=(16, 8), subplot_kw={"projection": "polar"})
+    fig, ax = plt.subplots(1, 3, figsize=(16, 8), subplot_kw={"projection": "polar"})
 
-path_1 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"
-plot_angular_velocity(
-    path_1, 10, ax[0], rotating=True, delta_omega=PARAMS["Delta_Omega"]
-)
+    path_1 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"
+    plot_angular_velocity(
+        path_1, 10, ax[0], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+    )
 
-path_2 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s3.h5"
-plot_angular_velocity(
-    path_2, 40, ax[1], rotating=True, delta_omega=PARAMS["Delta_Omega"]
-)
+    path_2 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s3.h5"
+    plot_angular_velocity(
+        path_2, 40, ax[1], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+    )
 
-path_3 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s4.h5"
-plot_angular_velocity(
-    path_3, 90, ax[2], rotating=True, delta_omega=PARAMS["Delta_Omega"]
-)
-plt.savefig("{}/Equator_spin_up_5e-2.png".format(args["fig_dir"]))
-plt.close()
+    path_3 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s4.h5"
+    plot_angular_velocity(
+        path_3, 90, ax[2], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+    )
+    plt.savefig("{}/Equator_spin_up_5e-2.png".format(args["fig_dir"]))
+    plt.close()
 
-path = "{}/su_equator/AZ_avg_equator".format(args["output_dir"])
-r_check, theta_check = get_angular_coords(path + "/AZ_avg_equator_s1.h5")
+    path = "{}/su_equator/AZ_avg_equator".format(args["output_dir"])
+    r_check, theta_check = get_angular_coords(path + "/AZ_avg_equator_s1.h5")
 
-r = LabeledCoordinate(r_check, "r")
-theta = LabeledCoordinate(theta_check, "theta")
+    r = LabeledCoordinate(r_check, "r")
+    theta = LabeledCoordinate(theta_check, "theta")
 
-path_list, fig = plot_against_time(r, "r", path, PARAMS["Ek"], PARAMS["Ntheta"])
-fig.savefig("{}/radial_against_time.png".format(args["fig_dir"]))
+    path_list, fig = plot_against_time(r, "r", path, PARAMS["Ek"], PARAMS["Ntheta"])
+    fig.savefig("{}/radial_against_time.png".format(args["fig_dir"]))
 
-if anim_check == "y":
-    num_files = len(path_list)
-    count = 0
-    Path.mkdir(args["frame_dir"], parents=True, exist_ok=True)
-    for i in range(num_files):
-        path = path_list[i]
-        data = h5py.File(path, mode="r")
-        time = np.array(data["scales/sim_time"])
-        for j in range(len(time)):
-            fig, ax = plt.subplots(
-                1, 1, figsize=(16, 8), subplot_kw={"projection": "polar"}
-            )
-            plot_angular_velocity(
-                path, j, ax, rotating=True, delta_omega=PARAMS["Delta_Omega"]
-            )
-            save_path = args["frame_dir"] / f"frame_spin_up_{count:04d}.png"
-            plt.savefig(save_path)
-            count = count + 1
-            if count % 20 == 0:
-                logger.info(f"saved frame {count:04d}.png")
+    if anim_check == "y":
+        num_files = len(path_list)
+        count = 0
+        Path.mkdir(args["frame_dir"], parents=True, exist_ok=True)
+        for i in range(num_files):
+            path = path_list[i]
+            data = h5py.File(path, mode="r")
+            time = np.array(data["scales/sim_time"])
+            for j in range(len(time)):
+                fig, ax = plt.subplots(
+                    1, 1, figsize=(16, 8), subplot_kw={"projection": "polar"}
+                )
+                plot_angular_velocity(
+                    path, j, ax, rotating=True, delta_omega=PARAMS["Delta_Omega"]
+                )
+                save_path = args["frame_dir"] / f"frame_spin_up_{count:04d}.png"
+                plt.savefig(save_path)
+                count = count + 1
+                if count % 20 == 0:
+                    logger.info(f"saved frame {count:04d}.png")

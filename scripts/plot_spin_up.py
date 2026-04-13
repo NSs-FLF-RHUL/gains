a4b2ee43ddc7a919e6816942f67dc1a3f77db3b3
Rebecca McShane <rmcshane29@gmail.com>
2026-04-13 15:01:52 +0100

    Refactor code so PARAMS dict is only created and called from script rather than module

--- scripts/plot_spin_up.py
+++ scripts/plot_spin_up.py
@@ -1,5 +1,6 @@
 """Analysis and plotting of the results of the single fluid spin up."""
 
+import json
 import logging
 import warnings
 from pathlib import Path
@@ -15,6 +16,7 @@ from gains.analysis.analyse_spin_up import (
     plot_against_time,
     plot_angular_velocity,
 )
+from gains.params.single_spin_up_rotating import parameters as default_params
 
 warnings.filterwarnings("ignore")
 logging.basicConfig(level=logging.INFO)
@@ -22,8 +24,6 @@ logging.basicConfig(level=logging.INFO)
 parser = create_parser()
 args = vars(parser.parse_args())
 
-# Alternatively if you prefer one-liners:
-# args = vars(create_parser().parse_args())
 
 if args["parameter_file"] is not None:
     with Path.open(args["parameter_file"]) as param_file:
@@ -44,13 +44,19 @@ anim_check = input("Plot frames for animation? [y/n]: ")
 fig, ax = plt.subplots(1, 3, figsize=(16, 8), subplot_kw={"projection": "polar"})
 
 path_1 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"
-plot_angular_velocity(path_1, 10, ax[0], rotating=True)
+plot_angular_velocity(
+    path_1, 10, ax[0], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+)
 
 path_2 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s3.h5"
-plot_angular_velocity(path_2, 40, ax[1], rotating=True)
+plot_angular_velocity(
+    path_2, 40, ax[1], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+)
 
 path_3 = args["output_dir"] / "su_equator/AZ_avg_equator/AZ_avg_equator_s4.h5"
-plot_angular_velocity(path_3, 90, ax[2], rotating=True)
+plot_angular_velocity(
+    path_3, 90, ax[2], rotating=True, delta_omega=PARAMS["Delta_Omega"]
+)
 plt.savefig("{}/Equator_spin_up_5e-2.png".format(args["fig_dir"]))
 plt.close()
 
@@ -60,7 +66,7 @@ r_check, theta_check = get_angular_coords(path + "/AZ_avg_equator_s1.h5")
 r = LabeledCoordinate(r_check, "r")
 theta = LabeledCoordinate(theta_check, "theta")
 
-path_list, fig = plot_against_time(r, "r", path)
+path_list, fig = plot_against_time(r, "r", path, PARAMS["Ek"], PARAMS["Ntheta"])
 fig.savefig("{}/radial_against_time.png".format(args["fig_dir"]))
 
 if anim_check == "y":
@@ -75,7 +81,9 @@ if anim_check == "y":
             fig, ax = plt.subplots(
                 1, 1, figsize=(16, 8), subplot_kw={"projection": "polar"}
             )
-            plot_angular_velocity(path, j, ax, rotating=True)
+            plot_angular_velocity(
+                path, j, ax, rotating=True, delta_omega=PARAMS["Delta_Omega"]
+            )
             save_path = args["frame_dir"] / f"frame_spin_up_{count:04d}.png"
             plt.savefig(save_path)
             count = count + 1

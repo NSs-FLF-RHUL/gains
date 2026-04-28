18973bef8ec780405a739aa5219a11479763a40d
Rebecca McShane <147627938+Rmc2564@users.noreply.github.com>
2026-04-28 10:23:43 +0100

    Adds optional time profiling to simulation code (#52)
    * Added decorator to use cProfile to analyse runtime and calls of each process
    
    * Profiling now saves to subdirectory in same directory as outputs
    
    * Fixed linting on the profiling decorator
    
    * Cleaner path handling in profile decorator
    
    * Apply suggestions from code review
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>
    
    * Removed additional check at end of file
    
    * Style fix
    
    ---------
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>

--- scripts/single_spin_up_rotating_frame.py
+++ scripts/single_spin_up_rotating_frame.py
@@ -1,11 +1,14 @@
 """Simulates the spin up of a full sphere containing a viscous newtonian fluid."""
 
 import argparse
+import cProfile
 import datetime
 import json
 import logging
+from collections.abc import Callable
 from pathlib import Path
 
+import dedalus.core as d3core
 import dedalus.public as d3
 import numpy as np
 from mpi4py import MPI
@@ -47,6 +50,15 @@ parser.add_argument(
     help="relative path to parameter file to use for this run, saved in json format.",
 )
 
+parser.add_argument(
+    "--profile",
+    type=str,
+    default=None,
+    help="If an arg is provided, will also generate time profiling data,"
+    " stored in a directory named as the"
+    " argument provided.",
+)
+
 args = vars(parser.parse_args())
 
 if args["parameter_file"] is not None:
@@ -64,13 +76,15 @@ PARAMS["output_dir"] = (
     else "single_spin_up_"
     + datetime.datetime.now().astimezone().strftime("%Y-%m-%m-%H:%M")
 )
+PARAMS["profile"] = args["profile"]
 # Additional Parameters - not likely to change between runs
 radius = 1
 timestepper = d3.SBDF2
 cfl_safety = 0.2
 max_timestep = 1e-2
 dtype = np.float64
-ncpu = MPI.COMM_WORLD.size
+comm = MPI.COMM_WORLD
+ncpu = comm.size
 log2 = np.log2(ncpu)
 
 if log2 == int(log2):
@@ -79,6 +93,45 @@ else:
     raise MeshError
 
 logger.info(f"running on processor mesh={mesh}")
+
+
+def profile(dirname: str | None) -> Callable:
+    """
+    Provide a decorator to use cProfile to profile an function running in parallel.
+
+    The stats are optionally saved in a subdirectory of the overall
+    output directory for the simulation, and saved using the dump_stats
+    method in a format readable by snakeviz.
+
+    :param dirname: The name of the directory to save the profiles to.
+    """
+    comm = MPI.COMM_WORLD
+
+    if dirname is None:
+        return lambda f: f
+
+    def prof_decorator(f: Callable) -> Callable:
+        def wrap_f(*args: object, **kwargs: object) -> object:
+            pr = cProfile.Profile()
+            pr.enable()
+            result = f(*args, **kwargs)
+            pr.disable()
+
+            output_dir = Path("outputs") / PARAMS["output_dir"] / dirname
+            # Only rank 0 creates directory to avoid race conditions
+            if comm.rank == 0:
+                output_dir.mkdir(parents=True, exist_ok=True)
+            # All ranks wait until directory exists
+            comm.Barrier()
+
+            filename = output_dir / Path(f"time_profile.{comm.rank}")
+            pr.dump_stats(filename)
+
+            return result
+
+        return wrap_f
+
+    return prof_decorator
 
 
 # Bases
@@ -150,7 +203,7 @@ problem = d3.IVP([p_n, u_n, tau_p_n, tau_u_n], namespace=locals())
 problem.add_equation("div(u_n) + tau_p_n = 0")
 problem.add_equation(
     "dt(u_n) + grad(p_n) - Ek*lap(u_n) + lift(tau_u_n)  = -u_n@grad(u_n) "
-    "-2*cross(ez,u_n) - cross(curl(u_n),u_n)"
+    "-2*cross(ez,u_n)"
 )
 problem.add_equation(
     "angular(u_n(r=radius)) = mask*angular(uang_r1) + (1-mask)*angular(u_n(r=radius))"
@@ -237,5 +290,11 @@ CFL.add_velocity(u_n)
 flow = d3.GlobalFlowProperty(solver, cadence=10)
 flow.add_property(np.sqrt(u_n @ u_n) * PARAMS["Ek"], name="Re_n")
 
-# Main loop
-solver.evolve(timestep_function=CFL.compute_timestep, log_cadence=10)
+
+@profile(dirname=PARAMS["profile"])
+def evolve(solver: d3core.solvers.InitialValueSolver) -> None:
+    """Call solver.evolve, but decorate with the profiling function."""
+    return solver.evolve(timestep_function=CFL.compute_timestep, log_cadence=10)
+
+
+evolve(solver)

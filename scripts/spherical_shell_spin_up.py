2eea6dc604744da400890f867aa196ae5fa67689
Rebecca McShane <rmcshane29@gmail.com>
2026-05-12 14:17:12 +0100

    Added neccessary substitutions and fields for problem

--- scripts/spherical_shell_spin_up.py
+++ scripts/spherical_shell_spin_up.py
@@ -71,15 +71,44 @@ sphere = basis_crust.outer_surface
 #Fields
 u_crust = basis_full.dist.VectorField(basis_full.coords, name="u_crust", bases=basis_crust)
 p_crust = basis_full.dist.VectorField(basis_full.coords, name = "p_crust", bases=basis_crust)
-tau_ucr_1 = basis_full.dist.VectorField(basis_full.coords, name = "tau_ucr_1", bases = basis_crust)
-tau_ucr_2 = basis_full.dist.VectorField(basis_full.coords, name = "tau_ucr_2", bases = basis_crust)
-tau_pcr = basis_full.dist.Field(name="tau_pcr", bases=basis_crust)
+tau_ucr_1 = basis_full.dist.VectorField(basis_full.coords, name = "tau_ucr_1", bases = sphere)
+tau_ucr_2 = basis_full.dist.VectorField(basis_full.coords, name = "tau_ucr_2", bases = sphere)
+tau_pcr = basis_full.dist.Field(name="tau_pcr")
 
 u_core = basis_full.dist.VectorField(basis_full.coords, name="u_core", bases=basis_core.ball)
 p_core = basis_full.dist.VectorField(basis_full.coords, name = "p_core", bases=basis_core.ball)
-tau_uco_1 = basis_full.dist.VectorField(basis_full.coords, name = "tau_uco_1", bases = basis_core.ball)
-tau_uco_2 = basis_full.dist.VectorField(basis_full.coords, name = "tau_uco_2", bases = basis_core.ball)
-tau_pco = basis_full.dist.Field(name="tau_pcr", bases=basis_core.ball)
+tau_uco_1 = basis_full.dist.VectorField(basis_full.coords, name = "tau_uco_1", bases = basis_core.sphere)
+tau_uco_2 = basis_full.dist.VectorField(basis_full.coords, name = "tau_uco_2", bases = basis_core.sphere)
+tau_pco = basis_full.dist.Field(name="tau_pco")
 
 phi, theta, r = basis_full.dist.local_grids(basis_full.ball)
-breakpoint()
+er = basis_full.dist.VectorField(basis_full.coords)
+etheta = basis_full.dist.VectorField(basis_full.coords)
+ephi = basis_full.dist.VectorField(basis_full.coords)
+
+er["g"][2] = 1
+etheta["g"][1] = 1
+ephi["g"][0] = 1
+
+ez = basis_full.dist.VectorField(basis_full.coords, bases=basis_full.ball)
+ez["g"][1] = -np.sin(theta)
+ez["g"][2] = np.cos(theta)  # unit vector in z direction
+
+sintheta = basis_full.dist.Field(name="sintheta", bases=basis_full.ball)
+sintheta["g"] = np.sin(theta)
+uang = basis_full.dist.VectorField(basis_full.coords, bases=basis_crust)(r=Ro).evaluate()
+uang["g"][0, :] = (PARAMS["Delta_Omega"] * sintheta)(r=Ro).evaluate()["g"]
+
+strain_rate_core = d3.grad(u_core) + d3.trans(d3.grad(u_core))
+shear_stress = d3.angular(d3.radial(strain_rate_core(r=1), index=1))
+r_vec_crust = basis_full.dist.VectorField(basis_full.coords, bases=basis_crust.radial_basis)
+r_vec_core = basis_full.dist.VectorField(basis_full.coords, bases = basis_core.ball.radial_basis)
+lift_basis_crust = basis_crust.derivative_basis(1)
+lift_basis_core = basis_core.ball.derivative_basis(1)
+
+lift_core = lambda a: d3.Lift(a, lift_basis_core, -1)
+lift_crust = lambda a: d3.Lift(a, lift_basis_crust, -1)
+
+grad_u_core = d3.grad(u_core) + r_vec_core * lift_core(tau_uco_1)
+grad_u_crust = d3.grad(u_crust) + r_vec_crust * lift_crust(tau_ucr_1)
+

f532e69910cc5f0bd416ad476b9d76921a89a519
Rebecca McShane <rmcshane29@gmail.com>
2026-05-18 16:52:48 +0100

    Added ShellBasis class

--- src/gains/problems/bases.py
+++ src/gains/problems/bases.py
@@ -26,9 +26,11 @@ class SphericalBasis:
         """
         Initialise spherical basis, including the surface.
 
-        :param mesh: cpu mesh for dedalus distributor.
-        :param dtype: data type for dedalus distributor.
-        :param radius: The radius of the sphere
+        :param coords: The spherical coordinates used for the simulation.
+        :param dist: The dedalus3 distributor used for the simulation.
+        :param dtype: Data type used for the simulation.
+        :param radius: Radius of the sphere (1 in non dimensionalised units).
+        :param params: Other simulation parameters
         """
         self.coords = coords
         self.dist = dist
@@ -36,6 +38,44 @@ class SphericalBasis:
             self.coords,
             shape=(params["Nphi"], params["Ntheta"], params["Nr"]),
             radius=radius,
+            dealias=params["dealias"],
+            dtype=dtype,
+        )
+
+
+class ShellBasis:
+    """Initialise bases and distributor to solve problems in shells in dedalus."""
+
+    coords: d3.SphericalCoordinates
+    dist: d3.Distributor
+    shell: d3.ShellBasis
+
+    @property
+    def surface(self) -> d3.SphereBasis:
+        """Define surface of the sphere."""
+        return self.shell.outer_surface
+
+    def __init__(
+        self,
+        coords: d3.SphericalCoordinates,
+        dist: d3.Distributor,
+        dtype: type,
+        **params: float,
+    ) -> None:
+        """
+        Initialise spherical basis, including the surface.
+
+        :param coords: The spherical coordinates used for the simulation.
+        :param dist: The dedalus3 distributor used for the simulation.
+        :param dtype: Data type used for the simulation.
+        :param params: Other simulation parameters
+        """
+        self.coords = coords
+        self.dist = dist
+        self.shell = d3.ShellBasis(
+            coords,
+            (params["Nphi"], params["Ntheta"], params["Nr"]),
+            radii=(params["Ri"], params["Ro"]),
             dealias=params["dealias"],
             dtype=dtype,
         )

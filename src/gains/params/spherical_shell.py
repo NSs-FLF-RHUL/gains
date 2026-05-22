2b5c4975ae37984442b672c99657019bd4173ac8
Rebecca McShane <147627938+Rmc2564@users.noreply.github.com>
2026-05-22 13:51:52 +0100

    Update src/gains/params/spherical_shell.py
    Add suggestion from review
    
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>

--- src/gains/params/spherical_shell.py
+++ src/gains/params/spherical_shell.py
@@ -2,16 +2,16 @@
 
 from typing import Any
 
-parameters = dict[str, Any]()
-parameters["Nphi"] = 128
-parameters["Ntheta"] = 64
-parameters["Nr"] = 64
-parameters["omega"] = 1
-parameters["Delta_Omega"] = 1e-3
-parameters["Ek"] = 5e-2
-parameters["Ri"] = 0.5
-parameters["Ro"] = 1.0
-parameters["B"] = 0.1
-
-parameters["dealias"] = 3 / 2
-parameters["stop_sim_time"] = 60
+parameters: dict[str, Any] = {
+  "Nphi" = 128,
+  "Ntheta" = 64,
+  "Nr" = 64,
+  "omega" = 1,
+  "Delta_Omega" = 1e-3,
+  "Ek" = 5e-2,
+  "Ri" = 0.5,
+  "Ro" = 1.0,
+  "B" = 0.1,
+  "dealias" = 3 / 2,
+  "stop_sim_time" = 60,
+ }

16afc0bde9c8837861067a95453f6e69a1f6c58e
Rebecca McShane <rmcshane29@gmail.com>
2026-05-22 13:55:38 +0100

    Fix value assignment in parameters dict

--- src/gains/params/spherical_shell.py
+++ src/gains/params/spherical_shell.py
@@ -3,15 +3,15 @@
 from typing import Any
 
 parameters: dict[str, Any] = {
-  "Nphi" = 128,
-  "Ntheta" = 64,
-  "Nr" = 64,
-  "omega" = 1,
-  "Delta_Omega" = 1e-3,
-  "Ek" = 5e-2,
-  "Ri" = 0.5,
-  "Ro" = 1.0,
-  "B" = 0.1,
-  "dealias" = 3 / 2,
-  "stop_sim_time" = 60,
- }
+    "Nphi": 128,
+    "Ntheta": 64,
+    "Nr": 64,
+    "omega": 1,
+    "Delta_Omega": 1e-3,
+    "Ek": 5e-2,
+    "Ri": 0.5,
+    "Ro": 1.0,
+    "B": 0.1,
+    "dealias": 3 / 2,
+    "stop_sim_time": 60,
+}

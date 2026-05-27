12f7d0b150cb0a7c8f37a55ea8fc7e9b99313d2b
Rebecca McShane <rmcshane29@gmail.com>
2026-05-27 14:33:57 +0100

    Added main loop to loggers module and added profiling

--- scripts/crust_core.py
+++ scripts/crust_core.py
@@ -1,30 +1,31 @@
 """
-Solve the HVBK equations for a spherical star subject to a boundary spin up.
+Solve Navier Stokes equations for 2 fluids coupled across a boundary.
 
-Equations and mutual friction are in the same form as
-J. R. Fuentes and Vanessa Graber 2024 ApJ 974 300.
+Both fluids have 0 penetration at the boundary. The crust fluid has 0 shear stress
+at the boundary, and the fluids match angular speed at the crust-core interface.
+The surface of the crust is spun up. 
+
+Each region is nondimensionalized using its own characteristic length scale, leading to
+distinct effective Ekman numbers in the core and shell.
 """
 
 import datetime
 import json
 import logging
 from pathlib import Path
+from collections.abc import Callable
 
 import dedalus.public as d3
 import numpy as np
-from dedalus.public import CrossProduct as Cross
-from dedalus.public import Curl
 from dedalus.public import DotProduct as Dot
 from mpi4py import MPI
 
 from gains.params.single_spin_up_rotating import parameters as default_params
-from dedalus.public import CrossProduct as Cross
-from dedalus.public import DotProduct as Dot
-from gains.problems.bases import SphericalBasis, ShellBasis
-from gains.utils.loggers import track_vorticity
+from gains.problems.bases import ShellBasis, SphericalBasis
 from gains.utils.misc import mesh_cpus
 from gains.utils.parsers import create_parser_simulation
 from gains.utils.profile import add_profiling_options, profile
+from gains.utils.loggers import track_reynolds_n
 
 # Setup
 logger = logging.getLogger(__name__)
@@ -62,8 +63,8 @@ max_timestep = 1e-2
 dtype = np.float64
 ncpu = MPI.COMM_WORLD.size
 
-Ek_shell = PARAMS["Ek"]*(PARAMS["Ro"]-PARAMS["Ri"])**2
-Ek_ball = PARAMS["Ek"]*PARAMS["Ri"]**2
+Ek_shell = PARAMS["Ek"] * (PARAMS["Ro"] - PARAMS["Ri"]) ** 2
+Ek_ball = PARAMS["Ek"] * PARAMS["Ri"] ** 2
 B = PARAMS["B"]
 Bprime = B / 2
 Ri = PARAMS["Ri"]
@@ -81,41 +82,41 @@ basis_core = SphericalBasis(coords, dist, dtype, Ri, **PARAMS)
 basis_crust = ShellBasis(coords, dist, dtype, **PARAMS)
 
 # Fields
-u_b = dist.VectorField(coords, name = "u_b", bases=basis_core.ball)
-p_b = dist.Field(name="p_b", bases = basis_core.ball)
+u_b = dist.VectorField(coords, name="u_b", bases=basis_core.ball)
+p_b = dist.Field(name="p_b", bases=basis_core.ball)
 tau_p_b = dist.Field(name="tau_p_b")
 tau_u_b_1 = dist.VectorField(coords, name="tau_u_b_1", bases=basis_core.sphere)
-tau_u_b_2 = dist.VectorField(coords, name = "tau_u_b_2", bases=basis_core.sphere)
+tau_u_b_2 = dist.VectorField(coords, name="tau_u_b_2", bases=basis_core.sphere)
 
-u_s = dist.VectorField(coords, name = "u_s", bases = basis_crust.shell)
-p_s = dist.Field(name = "p_s", bases=basis_crust.shell)
-tau_p_s = dist.Field(name = "tau_p_s")
-tau_u_s_1 = dist.VectorField(coords, name = "tau_u_s_1", bases = basis_crust.surface)
-tau_u_s_2 = dist.VectorField(coords, name = "tau_u_s_2", bases = basis_crust.surface)
+u_s = dist.VectorField(coords, name="u_s", bases=basis_crust.shell)
+p_s = dist.Field(name="p_s", bases=basis_crust.shell)
+tau_p_s = dist.Field(name="tau_p_s")
+tau_u_s_1 = dist.VectorField(coords, name="tau_u_s_1", bases=basis_crust.surface)
+tau_u_s_2 = dist.VectorField(coords, name="tau_u_s_2", bases=basis_crust.surface)
 
-#Substitutions - general
+# Substitutions - general
 er = dist.VectorField(coords)
 etheta = dist.VectorField(coords)
 ephi = dist.VectorField(coords)
 
-er['g'][2] = 1
-etheta['g'][1] = 1
-ephi['g'][0] = 1
+er["g"][2] = 1
+etheta["g"][1] = 1
+ephi["g"][0] = 1
 
-#Subsititutions: crust
+# Subsititutions for crust
 lift_basis_s = basis_crust.shell.derivative_basis(1)
 lift_s = lambda a: d3.Lift(a, lift_basis_s, -1)
 phi_s, theta_s, r_s = dist.local_grids(basis_crust.shell)
 ez_s = dist.VectorField(coords, bases=basis_crust.shell)
-ez_s['g'][1] = - np.sin(theta_s)
-ez_s['g'][2] = np.cos(theta_s)
+ez_s["g"][1] = -np.sin(theta_s)
+ez_s["g"][2] = np.cos(theta_s)
 
-rvec_s = dist.VectorField(coords, bases = basis_crust.shell.radial_basis)
-rvec_s['g'][2] = r_s
+rvec_s = dist.VectorField(coords, bases=basis_crust.shell.radial_basis)
+rvec_s["g"][2] = r_s
 
 grad_u_s = d3.grad(u_s) + rvec_s * lift_s(tau_u_s_1)
 stheta_s = dist.Field(name="stheta", bases=basis_crust.shell)
-stheta_s['g'] = np.sin(theta_s)
+stheta_s["g"] = np.sin(theta_s)
 uang_s = dist.VectorField(coords, bases=basis_crust.shell)(r=radius).evaluate()
 uang_s["g"][0, :] = (PARAMS["Delta_Omega"] * stheta_s)(r=radius).evaluate()["g"]
 
@@ -124,16 +125,16 @@ shear_stress_s_surface = d3.angular(d3.radial(strain_s(r=PARAMS["Ro"]), index=1)
 
 shear_stress_s_interface = d3.angular(d3.radial(strain_s(r=PARAMS["Ri"]), index=1))
 
-#Subsititutions: Core
+# Subsititutions for Core
 lift_b = lambda a: d3.Lift(a, basis_core.ball, -1)
 phi_b, theta_b, r_b = dist.local_grids(basis_core.ball)
 
 ez_b = dist.VectorField(coords, bases=basis_core.ball)
-ez_b['g'][1] = -np.sin(theta_b)
-ez_b['g'][2] = np.cos(theta_b)
+ez_b["g"][1] = -np.sin(theta_b)
+ez_b["g"][2] = np.cos(theta_b)
 
 rvec_b = dist.VectorField(coords, bases=basis_core.ball.radial_basis)
-rvec_b['g'][2] = r_b
+rvec_b["g"][2] = r_b
 
 grad_u_b = d3.grad(u_b) + rvec_b * lift_b(tau_u_b_1)
 
@@ -142,18 +143,8 @@ shear_stress_b_interface = d3.angular(d3.radial(strain_b(r=PARAMS["Ri"]), index=
 
 # Problem
 problem = d3.IVP(
-    [
-        u_s,
-        p_s,
-        tau_p_s,
-        tau_u_s_1,
-        tau_u_s_2,
-        u_b,
-        p_b,
-        tau_p_b,
-        tau_u_b_2
-    ],
-    namespace=locals()
+    [u_s, p_s, tau_p_s, tau_u_s_1, tau_u_s_2, u_b, p_b, tau_p_b, tau_u_b_2],
+    namespace=locals(),
 )
 
 problem.add_equation("trace(grad_u_s) + tau_p_s = 0")
@@ -161,11 +152,17 @@ problem.add_equation("div(u_b) + tau_p_b = 0")
 problem.add_equation("integ(p_b) = 0")
 problem.add_equation("integ(p_s) = 0")
 
-problem.add_equation("dt(u_b) - Ek_ball*lap(u_b) + grad(p_b) + lift_b(tau_u_b_2) = -u_b@grad(u_b) - 2*cross(ez_b, u_b)")
-problem.add_equation("dt(u_s) - Ek_shell*div(grad_u_s) + grad(p_s) + lift_s(tau_u_s_2) = -u_s@grad(u_s) - 2*cross(ez_s, u_s)")
+problem.add_equation(
+    "dt(u_b) - Ek_ball*lap(u_b) + grad(p_b) + lift_b(tau_u_b_2) = -u_b@grad(u_b) "
+    "- 2*cross(ez_b, u_b)"
+)
+problem.add_equation(
+    "dt(u_s) - Ek_shell*div(grad_u_s) + grad(p_s) + lift_s(tau_u_s_2) = -u_s@grad(u_s) "
+    "- 2*cross(ez_s, u_s)"
+)
 
-problem.add_equation("radial(u_s(r=Ro)) = 0") # Zero Penetration
-problem.add_equation("angular(u_s(r=Ro)) = angular(uang_s)") # Surface spin up
+problem.add_equation("radial(u_s(r=Ro)) = 0")  # Zero Penetration
+problem.add_equation("angular(u_s(r=Ro)) = angular(uang_s)")  # Surface spin up
 
 problem.add_equation("radial(u_s(r=Ri)) = 0")
 problem.add_equation("shear_stress_s_interface = 0")
@@ -204,6 +201,7 @@ def vol_avg(a: d3.Field) -> d3.Field:
     """Average over whole basis.sphere."""
     return d3.Integrate(a / volume, coords)
 
+
 u_b_r = Dot(u_b, er)
 u_b_theta = Dot(u_b, etheta)
 u_b_phi = Dot(u_b, ephi)
@@ -237,19 +235,8 @@ CFL.add_velocity(u_s)
 flow = d3.GlobalFlowProperty(solver, cadence=10)
 flow.add_property(np.sqrt(u_s @ u_s) * PARAMS["Ek"], name="Re_n")
 
-try:
-    logger.info("Starting main loop")
-    while solver.proceed:
-        timestep = CFL.compute_timestep()
-        solver.step(timestep)
-        if (solver.iteration - 1) % 10 == 0:
-            re = flow.max("Re_n")
-            logger.info(
-                "Iteration=%i, Time=%e, dt=%e, max(Re)=%f"
-                % (solver.iteration, solver.sim_time, timestep, re)
-            )
-except:
-    logger.exception("Exception raised, triggering end of main loop.")
-    raise
-finally:
-    solver.log_stats()
+@profile(args["profile"])
+def main() -> Callable:
+    return track_reynolds_n(logger, flow, solver, CFL)
+
+main()

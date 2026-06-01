19de18b202bcd5d5d9312f54e1e433ed8004b416
Rebecca McShane <rmcshane29@gmail.com>
2026-06-01 16:32:46 +0100

    Added the crust superfluid

--- scripts/crust_core.py
+++ scripts/crust_core.py
@@ -76,6 +76,8 @@ Ro = PARAMS["Ro"]
 radius = Ro
 x_b_n = 0.05 # Proton fraction - core
 x_b_s = 0.95 # Neutron fraction - core
+x_s_n = 0.05
+x_s_s = 0.95
 
 mesh = mesh_cpus(ncpu)
 
@@ -104,11 +106,18 @@ tau_u_b_s_1 = dist.VectorField(coords, name="tau_u_b_s_1", bases=basis_core.sphe
 tau_u_b_s_2 = dist.VectorField(coords, name="tau_u_b_s_2", bases=basis_core.sphere)
 
 # Crust (shell basis)
-u_s = dist.VectorField(coords, name="u_s", bases=basis_crust.shell)
-p_s = dist.Field(name="p_s", bases=basis_crust.shell)
-tau_p_s = dist.Field(name="tau_p_s")
-tau_u_s_1 = dist.VectorField(coords, name="tau_u_s_1", bases=basis_crust.surface)
-tau_u_s_2 = dist.VectorField(coords, name="tau_u_s_2", bases=basis_crust.surface)
+u_s_n = dist.VectorField(coords, name="u_s_n", bases=basis_crust.shell)
+p_s_n = dist.Field(name="p_s_n", bases=basis_crust.shell)
+u_s_s = dist.VectorField(coords, name = "u_s_s", bases = basis_crust.shell)
+p_s_s = dist.Field(name="p_s_s", bases=basis_crust.shell)
+
+tau_p_s_n = dist.Field(name="tau_p_s_n")
+tau_u_s_n_1 = dist.VectorField(coords, name="tau_u_s_n_1", bases=basis_crust.surface)
+tau_u_s_n_2 = dist.VectorField(coords, name="tau_u_s_n_2", bases=basis_crust.surface)
+
+tau_p_s_s = dist.Field(name="tau_p_s_s")
+tau_u_s_s_1 = dist.VectorField(coords, name="tau_u_s_s_1", bases=basis_crust.surface)
+tau_u_s_s_2 = dist.VectorField(coords, name="tau_u_s_s_2", bases=basis_crust.surface)
 
 # Substitutions - general
 er = dist.VectorField(coords)
@@ -130,16 +139,29 @@ ez_s["g"][2] = np.cos(theta_s)
 rvec_s = dist.VectorField(coords, bases=basis_crust.shell.radial_basis)
 rvec_s["g"][2] = r_s
 
-grad_u_s = d3.grad(u_s) + rvec_s * lift_s(tau_u_s_1)
+grad_u_s_n = d3.grad(u_s_n) + rvec_s * lift_s(tau_u_s_n_1)
+grad_u_s_s = d3.grad(u_s_s) + rvec_s * lift_s(tau_u_s_s_1)
+
 stheta_s = dist.Field(name="stheta", bases=basis_crust.shell)
 stheta_s["g"] = np.sin(theta_s)
 uang_s = dist.VectorField(coords, bases=basis_crust.shell)(r=radius).evaluate()
 uang_s["g"][0, :] = (PARAMS["Delta_Omega"] * stheta_s)(r=radius).evaluate()["g"]
 
-strain_s = grad_u_s + d3.trans(grad_u_s)
-shear_stress_s_surface = d3.angular(d3.radial(strain_s(r=PARAMS["Ro"]), index=1))
+strain_s_n = grad_u_s_n + d3.trans(grad_u_s_n)
+strain_s_s = grad_u_s_s + d3.trans(grad_u_s_s)
+shear_stress_s_n_surface = d3.angular(d3.radial(strain_s_n(r=PARAMS["Ro"]), index=1))
 
-shear_stress_s_interface = d3.angular(d3.radial(strain_s(r=PARAMS["Ri"]), index=1))
+shear_stress_s_n_interface = d3.angular(d3.radial(strain_s_n(r=PARAMS["Ri"]), index=1))
+
+shear_stress_s_s_interface = d3.angular(d3.radial(strain_s_s(r=PARAMS["Ri"]), index=1))
+shear_stress_s_s_surface = d3.angular(d3.radial(strain_s_s(r=PARAMS["Ro"]), index=1))
+
+omega_s_s = dist.VectorField(coords, name="omega_s", bases=basis_core.ball) # Superfluid vorticity
+
+u_s_ns = u_s_n - u_s_s
+omega_s_s = Curl(u_s_s) + 2 * ez_s
+omega_unit_s = omega_s_s / 2
+F_mf_s = B * (Cross(omega_unit_s, Cross(omega_s_s, u_s_ns))) + Bprime * Cross(omega_s_s, u_s_ns)
 
 # Subsititutions for Core
 lift_b = lambda a: d3.Lift(a, basis_core.ball, -1)
@@ -155,47 +177,76 @@ rvec_b["g"][2] = r_b
 strain_b_s = d3.grad(u_b_s) + d3.trans(d3.grad(u_b_s))
 shear_stress_b_s_interface = d3.angular(d3.radial(strain_b_s(r=PARAMS["Ri"]), index=1))
 
-omega_b_s = dist.VectorField(coords, name="omega_s", bases=basis_core.ball) # Superfluid vorticity
+omega_b_s = dist.VectorField(coords, name="omega_b_s", bases=basis_core.ball) # Superfluid vorticity
 
 u_b_ns = u_b_n - u_b_s
 omega_b_s = Curl(u_b_s) + 2 * ez_b
-omega_unit = omega_b_s / 2
-F_mf = B * (Cross(omega_unit, Cross(omega_b_s, u_b_ns))) + Bprime * Cross(omega_b_s, u_b_ns)
+omega_unit_b = omega_b_s / 2
+F_mf_b = B * (Cross(omega_unit_b, Cross(omega_b_s, u_b_ns))) + Bprime * Cross(omega_b_s, u_b_ns)
 
 # Problem
 problem = d3.IVP(
-    [u_s, p_s, tau_p_s, tau_u_s_1, tau_u_s_2, u_b_n, p_b_n, tau_p_b_n, tau_u_b_n_2, u_b_s, p_b_s, tau_p_b_s, tau_u_b_s_2],
+    [u_s_n,
+     p_s_n,
+     tau_p_s_n,
+     tau_u_s_n_1,
+     tau_u_s_n_2,
+     u_s_s,
+     p_s_s,
+     tau_p_s_s,
+     tau_u_s_s_1,
+     tau_u_s_s_2,
+     u_b_n,
+     p_b_n,
+     tau_p_b_n,
+     tau_u_b_n_2,
+     u_b_s,
+     p_b_s,
+     tau_p_b_s,
+     tau_u_b_s_2
+     ],
     namespace=locals(),
 )
 
-problem.add_equation("trace(grad_u_s) + tau_p_s = 0")
+problem.add_equation("trace(grad_u_s_n) + tau_p_s_n = 0")
+problem.add_equation("trace(grad_u_s_s) + tau_p_s_s = 0")
 problem.add_equation("div(u_b_n) + tau_p_b_n = 0")
 problem.add_equation("div(u_b_s) + tau_p_b_s = 0")
 problem.add_equation("integ(p_b_n) = 0")
-problem.add_equation("integ(p_s) = 0")
+problem.add_equation("integ(p_s_n) = 0")
 problem.add_equation("integ(p_b_s) = 0")
+problem.add_equation("integ(p_s_s) = 0")
 
 problem.add_equation(
     "dt(u_b_n) - Ek_ball*lap(u_b_n) + grad(p_b_n) + lift_b(tau_u_b_n_2) = -u_b_s@grad(u_b_n) "
-    "- 2*cross(ez_b, u_b_n) + x_b_s/x_b_n * F_mf"
+    "- 2*cross(ez_b, u_b_n) + x_b_s/x_b_n * F_mf_b"
 )
 
 problem.add_equation("dt(u_b_s) + grad(p_b_s) + lift_b(tau_u_b_s_2) = -u_b_s@grad(u_b_s)"
-                     " - 2*cross(ez_b,u_b_s) - F_mf")
+                     " - 2*cross(ez_b,u_b_s) - F_mf_b")
 
 problem.add_equation(
-    "dt(u_s) - Ek_shell*div(grad_u_s) + grad(p_s) + lift_s(tau_u_s_2) = -u_s@grad(u_s) "
-    "- 2*cross(ez_s, u_s)"
+    "dt(u_s_n) - Ek_shell*div(grad_u_s_n) + grad(p_s_n) + lift_s(tau_u_s_n_2) = -u_s_n@grad(u_s_n) "
+    "- 2*cross(ez_s, u_s_n) + x_s_s/x_s_n * F_mf_s"
 )
 
-problem.add_equation("radial(u_s(r=Ro)) = 0")  # Zero Penetration
-problem.add_equation("angular(u_s(r=Ro)) = angular(uang_s)")  # Surface spin up
+problem.add_equation(
+    "dt(u_s_s) + grad(p_s_s) + lift_s(tau_u_s_s_2) = -u_s_s@grad(u_s_s) "
+    "- 2* cross(ez_s, u_s_s) - F_mf_s"
+)
 
-problem.add_equation("radial(u_s(r=Ri)) = 0")
-problem.add_equation("shear_stress_s_interface = 0")
+problem.add_equation("radial(u_s_n(r=Ro)) = 0")  # Zero Penetration
+problem.add_equation("angular(u_s_n(r=Ro)) = angular(uang_s)")  # Surface spin up
+problem.add_equation("radial(u_s_s(r=Ro)) = 0")
+problem.add_equation("shear_stress_s_s_surface = 0")
+
+problem.add_equation("radial(u_s_n(r=Ri)) = 0")
+problem.add_equation("shear_stress_s_n_interface = 0")
+problem.add_equation("radial(u_s_s(r=Ri)) = 0")
+problem.add_equation("shear_stress_s_s_interface = 0")
 
 problem.add_equation("radial(u_b_n(r=Ri)) = 0")
-problem.add_equation("angular(u_b_n(r=Ri)) = angular(u_s(r=Ri))")
+problem.add_equation("angular(u_b_n(r=Ri)) = angular(u_s_n(r=Ri))")
 
 problem.add_equation("radial(u_b_s(r=Ri)) = 0")
 problem.add_equation("shear_stress_b_s_interface = 0")
@@ -207,12 +258,14 @@ if PARAMS["use_checkpoint"]:
     write, timestep = solver.load_state(PARAMS["checkpoint_path"])
 else:
     # Initial condition
-    u_s.fill_random("g", seed=42, distribution="normal", scale=1e-10)  # Random noise
-    u_s.low_pass_filter(scales=0.5)
+    u_s_n.fill_random("g", seed=42, distribution="normal", scale=1e-10)  # Random noise
+    u_s_n.low_pass_filter(scales=0.5)
     u_b_n.fill_random("g", seed=67, distribution="normal", scale=1e-10)  # Random noise
     u_b_n.low_pass_filter(scales=0.5)
     u_b_s.fill_random("g", seed=42, distribution="normal", scales=1e-10)
     u_b_s.low_pass_filter(scales=0.5)
+    u_s_s.fill_random("g", seed=67, distribution="normal", scale=1e-10)
+    u_s_s.low_pass_filter(scales=0.5)
     timestep = max_timestep
 
 # Analysis
@@ -238,9 +291,9 @@ u_b_n_r = Dot(u_b_n, er)
 u_b_n_theta = Dot(u_b_n, etheta)
 u_b_n_phi = Dot(u_b_n, ephi)
 
-u_s_r = Dot(u_s, er)
-u_s_theta = Dot(u_s, etheta)
-u_s_phi = Dot(u_s, ephi)
+u_s_n_r = Dot(u_s_n, er)
+u_s_n_theta = Dot(u_s_n, etheta)
+u_s_n_phi = Dot(u_s_n, ephi)
 
 save_path = Path("outputs/{}/su_equator".format(PARAMS["output_dir"]))
 save_path.mkdir(parents=True, exist_ok=True)
@@ -254,18 +307,18 @@ AZ_avg.add_task(az_avg(u_b_n_r), name="u_b_r")
 AZ_avg.add_task(az_avg(u_b_n_theta), name="u_b_theta")
 AZ_avg.add_task(az_avg(u_b_n_phi), name="u_b_phi")
 
-AZ_avg.add_task(az_avg(u_s_r), name="u_s_r")
-AZ_avg.add_task(az_avg(u_s_theta), name="u_s_theta")
-AZ_avg.add_task(az_avg(u_s_phi), name="u_s_phi")
+AZ_avg.add_task(az_avg(u_s_n_r), name="u_s_r")
+AZ_avg.add_task(az_avg(u_s_n_theta), name="u_s_theta")
+AZ_avg.add_task(az_avg(u_s_n_phi), name="u_s_phi")
 
 CFL = d3.CFL(
     solver, timestep, cadence=1, safety=0.5, threshold=0.1, max_dt=max_timestep
 )
 CFL.add_velocity(u_b_n)
-CFL.add_velocity(u_s)
+CFL.add_velocity(u_s_n)
 
 flow = d3.GlobalFlowProperty(solver, cadence=10)
-flow.add_property(np.sqrt(u_s @ u_s) * PARAMS["Ek"], name="Re_n")
+flow.add_property(np.sqrt(u_s_n @ u_s_n) * PARAMS["Ek"], name="Re_n")
 
 
 @profile(args["profile"], PARAMS)

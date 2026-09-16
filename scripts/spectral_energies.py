Fix energy spectrum calculation

scripts/spectral_energies.py
@@ -5,11 +5,9 @@ import dedalus.public as d3
 from pastamarkers import markers
 from rich.progress import Progress, TextColumn, BarColumn
 
-paths = ["outputs/gaussian_circ_crust/su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5",
-         "outputs/gaussian_circ_crust/su_equator/AZ_avg_equator/AZ_avg_equator_s2.h5",
-         "outputs/gaussian_circ_crust/su_equator/AZ_avg_equator/AZ_avg_equator_s3.h5"]
+paths = ["outputs/artificial_viscosity/su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"]
 
-#paths = ["outputs/spectral_filter/su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"]
+#paths = ["outputs/dealias_2/su_equator/AZ_avg_equator/AZ_avg_equator_s1.h5"]
 
 Nphi = 128
 Ntheta = 64
@@ -44,13 +42,15 @@ with Progress(
                 u_theta['g'] = u_theta_series[increment]
                 u_phi['g'] = u_phi_series[increment]
                 time = int(time)
+                '''
                 energy_grid = 0.5 * (u_r_series[increment]**2 +  u_theta_series[increment]**2 + u_phi_series[increment]**2)
                 energy = dist.Field(name="energy", bases=basis)
                 energy['g'] = energy_grid
                 energy_coeff = energy['c']
                 power_spec = np.abs(energy_coeff**2)
-
                 '''
+                
+                
                 u_r.change_scales(1)
                 u_theta.change_scales(1)
                 u_phi.change_scales(1)
@@ -59,14 +59,13 @@ with Progress(
                 u_phi_coeff = u_phi['c']
 
                 energy_density = np.abs(u_r_coeff)**2 + np.abs(u_theta_coeff)**2 + np.abs(u_phi_coeff)**2
-                energy_sum_r = np.sum(energy_density, axis=2)
-                E_l = np.sum(energy_sum_r, axis=0)
-                E_m = np.sum(energy_sum_r, axis=1)
-                '''
 
-                E_m = np.sum(power_spec, axis=(1,2))
-                E_l = np.sum(power_spec, axis=(0,2))
-                E_n = np.sum(power_spec, axis=(0,1))
+                
+
+                
+                E_m = np.sum(energy_density, axis=(1,2))
+                E_l = np.sum(energy_density, axis=(0,2))
+                E_n = np.sum(energy_density, axis=(0,1))
 
                 l_axis = np.arange(len(E_l))
                 m_axis = np.arange(len(E_m))
@@ -75,21 +74,19 @@ with Progress(
                 #plt.yscale('log')
                 plt.xlabel("l")
                 plt.ylabel(r"$E_{l}$")
-                plt.savefig(f"/Users/rebecca/Desktop/Spectra_time_series/l/t={time:04d}")
+                plt.savefig(f"outputs/artificial_viscosity/l_n/t={time:04d}")
                 plt.close()
 
 
                 plt.loglog(m_axis[1:], E_m[1:], 'x', markersize=3.0, c='black')
                 plt.xlabel("m")
                 plt.ylabel(r"$E_{m}$")
-                plt.savefig(f"/Users/rebecca/Desktop/Spectra_time_series/m/t={time:04d}")
+                plt.savefig(f"outputs/artificial_viscosity/m_n/t={time:04d}")
                 plt.close()
 
                 uphi_circ = u_phi['g'][0, :, 32]
                 plt.plot(theta, uphi_circ)
-                plt.savefig(f"/Users/rebecca/Desktop/Spectra_time_series/uphi/t={time:04d}")
+                plt.savefig(f"outputs/artificial_viscosity/uphi_n/t={time:04d}")
                 plt.close()
                 p.advance(task)
                 increment += 1
-                if increment == 198:
-                    breakpoint()